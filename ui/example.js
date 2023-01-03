770742663b18a0f1a05f7220b5db8c0d8d35653e
weartist <weartistt@gmail.com>
2023-01-03 19:53:12 +0800

    remove necessary files

--- ui/example.js
+++
@@ -1,6 +0,0 @@
-window.__TAURI__.event.listen('keyDown', (event) => {
-	console.log('receive event ' + event);
-	let div = document.querySelector('h1')
-	console.log(event.payload.message)
-	div.innerText = event.payload.message
-})
