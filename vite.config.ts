648d03be76d53854f2632ec539866d84987b3eeb
weartist <weartistt@gmail.com>
2023-02-04 12:16:12 +0800

    add vite support

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,9 +17,11 @@ export default defineConfig({
   build: {
     // Tauri supports es2021
     target: ["es2021", "chrome100", "safari13"],
+    // target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
     // don't minify for debug builds
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
   },
+  plugins: [react()],
 });
